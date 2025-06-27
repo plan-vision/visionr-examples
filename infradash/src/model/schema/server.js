df31c6775beb9d5e8e1eb6d77eadab27fce59d87
stelf <larodi@gmail.com>
2025-06-27 16:10:40 +0300

    events

--- infradash/src/model/schema/server.js
+++ infradash/src/model/schema/server.js
@@ -3,84 +3,104 @@ vr.defineI18n(require('./server.i18n.json'));
 module.exports = {
 	icon: 'folder_special',
 	hierarchies: [
-        {
-            name: {
-                "en-US": "By collocation",
-				"fr-FR": "Par Colocation",
-                "bg-BG": "По колокация",
-                "de-DE": "Nach Colocation"
-            },
-            code: 'infra.colocation.racks.servers',
-            path: 'infra.colocation.racks.servers',
-            showElementCount: true,
-            // defaultRecursive: true,
-            // expandInitialDepth: 4,
-            // showLeafs: true
-        }, 
+		{
+			name: {
+				"en-US": "By colocation and rack",
+				"fr-FR": "Par colocation et rack",
+				"bg-BG": "По колокация и rack",
+				"de-DE": "Nach Colocation und Rack"
+			},
+			code: 'infra.colocation.racks.servers',
+			path: 'infra.colocation.racks.servers',
+			showElementCount: true,
+			// defaultRecursive: true,
+			// expandInitialDepth: 4,
+			// showLeafs: true
+		},
 
-    ],
+	],
 	properties: {
 		/* BY CATEGORY */
 		'main.basic': {
-		  'objectdef': 'objectdef.hidden',
-		  'icon': false,
-		  'sort_id': 'sortid.hidden',
-		  'rack': {
-			template: 'relation.obligatory',
-			related: 'infra.rack'
-		  },
-		  'name': {
-			template: 'text.i18n',
-		  },
-		  'model': {
-			template: 'text'
-		  },
-		  'serial_number': {
-			template: 'text'
-		  },
-		  'cpu_count': {
-			template: 'integer',
-			default: 1
-		  },
-		  'cpu_speed': { 
-			template: 'integer',
-			unit: 'MHz'	
-		  },
-		  'memory': {
-			template: 'integer',
-			unit: 'GB'
-		  },
-		  'storage': {
-			template: 'text'
-		  },
-		  'status': {
-			template: 'option.obligatory',
-			optionSet : {
-				code : 'server_status',
-				options : ['online','offline','booting','stopping','error','poweroff']
+			'objectdef': 'objectdef.hidden',
+			'icon': false,
+			'sort_id': 'sortid.hidden',
+			'rack': {
+				template: 'relation.obligatory',
+				related: 'infra.rack',
+				events: {
+					'insert,update'() {
+						console.log('Server rack insert/update event triggered.');
+						for(const c of this.components) {
+							c.colocation = this.rack.colocation;
+							console.log(`Component ${c.name} colocation updated to ${c.colocation}`);
+						}
+					},
+					delete() {
+						console.log('Server rack delete event triggered.');
+						for(const c of this.components) {
+							c.colocation = undefined;
+							console.log(`Component ${c.name} colocation set to undefined`);
+						}
+					}
+				}
 			},
-		  },
-		  'last_updated': {
-			template: 'datetime'
-		  },
-		  'physical_admin': {
-			template: 'relation',
-			related: 'contacts.employee'
-		  },
-		  'vendor': {
-			template: 'relation',
-			related: 'contacts.vendor'
-		  },
-		  'services': {
-			template: 'relation',
-			parent: 'infra.service.server'
-		  },
-		  'incidents': {
-			template: 'relation',
-			parent: 'infra.incident.server'
-		  },
+			'name': {
+				template: 'text.i18n',
+			},
+			'model': {
+				template: 'text'
+			},
+			'serial_number': {
+				template: 'text'
+			},
+			'cpu_count': {
+				template: 'integer',
+				default: 1
+			},
+			'cpu_speed': {
+				template: 'integer',
+				unit: 'MHz'
+			},
+			'memory': {
+				template: 'integer',
+				unit: 'GB'
+			},
+			'storage': {
+				template: 'text'
+			},
+			'status': {
+				template: 'option.obligatory',
+				optionSet: {
+					code: 'server_status',
+					options: ['online', 'offline', 'booting', 'stopping', 'error', 'poweroff']
+				},
+			},
+			'last_updated': {
+				template: 'datetime'
+			},
+			'physical_admin': {
+				template: 'relation',
+				related: 'contacts.employee'
+			},
+			'vendor': {
+				template: 'relation',
+				related: 'contacts.vendor'
+			},
+			'services': {
+				template: 'relation',
+				parent: 'infra.service.server'
+			},
+			'incidents': {
+				template: 'relation',
+				parent: 'infra.incident.server'
+			},
+			'components': {
+				template: 'relation',
+				parent: 'infra.component.server'
+			},
 		}
-	  },
+	},
 	events: {
 
 	},
